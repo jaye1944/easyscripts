379d8f038e22f0bc06d5bbcefbad3d49ea31d2c4
jaye1944 <jaye1944@gmail.com>
2015-09-05 21:41:32 +0530

    add file exist check

--- cplog.sh
+++ cplog.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-logFile='a'
-outFile='out'
+logFile='infile'
+outFile='outfile'
+#check file is exist
+if ! [ -e "$logFile" ]
+then
+  echo "File $logFile  NOT there"
+  exit 0
+fi
+
 #chk num of args
 if [ $# -eq 0 ];
 then
-echo "Usage is cplog [start] [end]"
+echo "Usage cplog [start] [end]"
 exit 0
 fi
 
@@ -14,6 +21,8 @@ then
 echo "[start] < [end]"
 exit 0
 fi
-en=$((($2+1)-$1))
-awk 'NR>='$1 $logFile | awk 'NR<='$en > $outFile
+en=$((($2+1)-$1)) #get limit
+awk 'NR>='$1 $logFile | awk 'NR<='$en > $outFile #exract correct part
+
+#scp $outFile user@127.0.0.0:~/location
 
